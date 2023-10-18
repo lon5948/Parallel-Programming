dc7356c2aee2078976fa42daab0cf4537ea932c7
lon5948 <a0988282303@gmail.com>
2023-10-18 22:54:07 +0800

    part1: clampedExpVector finish

--- hw1/part1/vectorOP.cpp
+++ hw1/part1/vectorOP.cpp
@@ -42,15 +42,52 @@ void absVector(float *values, float *output, int N)
 
 void clampedExpVector(float *values, int *exponents, float *output, int N)
 {
-  //
-  // PP STUDENTS TODO: Implement your vectorized version of
-  // clampedExpSerial() here.
-  //
-  // Your solution should work for any value of
-  // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
-  //
+  __pp_vec_float x, result;
+  __pp_vec_int y, count;
+  __pp_vec_int zero = _pp_vset_int(0);
+  __pp_vec_int one = _pp_vset_int(1);
+  __pp_vec_float maxFloat = _pp_vset_float(9.999999f);
+  __pp_mask maskAll, maskIsEqualZero, maskIsNotEqualZero, maskIsPositive, maskIsBiggerThanMaxFloat;
+
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
+    if (i + VECTOR_WIDTH > N)
+      maskAll = _pp_init_ones(N - i);
+    else
+      maskAll = _pp_init_ones();
+    maskIsEqualZero = _pp_init_ones(0);
+    maskIsNotEqualZero = _pp_init_ones(0);
+    maskIsPositive = _pp_init_ones(0);
+    maskIsBiggerThanMaxFloat = _pp_init_ones(0);
+
+    _pp_vload_float(x, values + i, maskAll); // x = values[i];
+
+    _pp_vload_int(y, exponents + i, maskAll); // y = exponents[i];
+
+    _pp_veq_int(maskIsEqualZero, y, zero, maskAll); // if (y == 0)
+
+    _pp_vset_float(result, 1.f, maskIsEqualZero); // output[i] = 1.f
+
+    maskIsNotEqualZero = _pp_mask_not(maskIsEqualZero); // else
+
+    _pp_vmove_float(result, x, maskIsNotEqualZero); // result = x;
+
+    _pp_vsub_int(count, y, one, maskIsNotEqualZero); // count = y - 1;
+
+    // if count > 0
+    _pp_vgt_int(maskIsPositive, count, zero, maskIsNotEqualZero); 
+    
+    while (_pp_cntbits(maskIsPositive)) {
+      _pp_vmult_float(result, result, x, maskIsPositive);	// result *= x;
+	    _pp_vsub_int(count, count, one, maskIsPositive);	// count--;
+	    _pp_vgt_int(maskIsPositive, count, zero, maskIsPositive);
+    }
+
+    _pp_vgt_float(maskIsBiggerThanMaxFloat, result, maxFloat, maskAll); // if (result > 9.999999f)
+
+    _pp_vset_float(result, 9.999999f, maskIsBiggerThanMaxFloat);   // output[i] = 9.999999f;
+
+    _pp_vstore_float(output + i, result, maskAll);
   }
 }
 
