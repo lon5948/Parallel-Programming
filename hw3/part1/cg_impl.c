b108d43911116702c3859b82d086f1e84df851f4
lon5948 <a0988282303@gmail.com>
2023-11-19 13:33:17 +0800

    Part1: parallelizing conjugate gradient method with OpenMP

--- hw3/part1/cg_impl.c
+++ hw3/part1/cg_impl.c
@@ -57,6 +57,7 @@ void conj_grad(int colidx[],
         //       unrolled-by-two version is some 10% faster.
         //       The unrolled-by-8 version below is significantly faster
         //       on the Cray t3d - overall speed of code is 1.5 times faster.
+        #pragma omp parallel for private(j, k, sum)
         for (j = 0; j < lastrow - firstrow + 1; j++)
         {
             sum = 0.0;
