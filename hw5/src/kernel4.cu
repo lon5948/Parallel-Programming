5b484c87828e5806d0b6d8f46d09749a19d93f90
lon5948 <a0988282303@gmail.com>
2023-12-16 19:50:04 +0800

    [hw5] kernel4.cu finish

---
+++ hw5/src/kernel4.cu
@@ -0,0 +1,56 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCK_SIZE 16
+
+__global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY, int *d_img, int resX, int maxIterations) {
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+    int thisX = blockIdx.x * blockDim.x + threadIdx.x;
+    int thisY = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int idx = thisY * resX + thisX;
+    
+    float c_re = lowerX + thisX * stepX;
+    float c_im = lowerY + thisY * stepY;
+    float z_re = c_re, z_im = c_im;
+
+    int i;
+    for (i = 0; i < maxIterations; ++i) {
+
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+
+    d_img[idx] = i;
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX;
+    float stepY = (upperY - lowerY) / resY;
+
+    int size = resX * resY * sizeof(int);
+
+    cudaHostRegister((void *)img, size, cudaHostRegisterMapped);
+
+    int *d_img;
+    cudaHostGetDevicePointer(&d_img, img, 0);
+    
+    dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 numBlocks(resX / BLOCK_SIZE, resY / BLOCK_SIZE);
+
+    mandelKernel<<<numBlocks, threadsPerBlock>>>(lowerX, lowerY, stepX, stepY, d_img, resX, maxIterations);
+    
+    cudaDeviceSynchronize();
+    cudaHostUnregister((void *)img);
+}
