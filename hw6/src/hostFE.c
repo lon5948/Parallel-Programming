e2948a2087ec2cd5fddd12f6becfcc8d6fe0577b
lon5948 <a0988282303@gmail.com>
2024-01-04 19:33:49 +0800

    hw6: revise convolution

--- hw6/src/hostFE.c
+++ hw6/src/hostFE.c
@@ -8,16 +8,17 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             cl_context *context, cl_program *program)
 {
     cl_int status;
-    
-    int imageSize = imageHeight * imageWidth;
-    size_t dataSize = imageSize * sizeof(float);
+    size_t imageSize = imageHeight * imageWidth * sizeof(float);
     size_t filterSize = filterWidth * filterWidth * sizeof(float);
     
     cl_command_queue commandQueue = clCreateCommandQueue(*context, *device, 0, &status);
-    
-    cl_mem inputBuffer = clCreateBuffer(*context, CL_MEM_USE_HOST_PTR, dataSize, inputImage, &status);
-    cl_mem filterBuffer = clCreateBuffer(*context, CL_MEM_USE_HOST_PTR, filterSize, filter, &status);
-    cl_mem outputBuffer = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, dataSize, NULL, &status);
+
+    cl_mem filterBuffer = clCreateBuffer(*context, 0, filterSize, NULL, &status);
+    cl_mem inputBuffer = clCreateBuffer(*context, 0, imageSize, NULL, &status);
+    cl_mem outputBuffer = clCreateBuffer(*context, 0, imageSize, NULL, &status);
+
+    clEnqueueWriteBuffer(commandQueue, inputBuffer, CL_MEM_READ_ONLY, 0, imageSize, inputImage, 0, NULL, NULL);
+    clEnqueueWriteBuffer(commandQueue, filterBuffer, CL_MEM_READ_ONLY, 0, filterSize, filter, 0, NULL, NULL);
 
     cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
 
@@ -28,10 +29,9 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     clSetKernelArg(kernel, 4, sizeof(cl_int), (void *)&imageWidth);
     clSetKernelArg(kernel, 5, sizeof(cl_int), (void *)&filterWidth);
 
-    size_t global_work_size = imageSize;
-    size_t local_work_size = 64;
-    clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL);
-    clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0, dataSize, outputImage, 0, NULL, NULL);
+    size_t globalSize[2] = {imageWidth, imageHeight};
+    clEnqueueNDRangeKernel(commandQueue, kernel, 2, NULL, globalSize, NULL, 0, NULL, NULL);
+    clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0, imageSize, outputImage, 0, NULL, NULL);
 
     clReleaseCommandQueue(commandQueue);
     clReleaseMemObject(inputBuffer);
--- hw6/src/kernel.cl
+++ hw6/src/kernel.cl
@@ -1,28 +1,24 @@
 __kernel void convolution(const __global float *inputImage, __global float *outputImage, __constant float *filter,
                           const int imageHeight, const int imageWidth, const int filterWidth) 
 {
-   int idx = get_global_id(0);
-   int row = idx / imageWidth;
-   int col = idx % imageWidth;
-   int halfFilterSize = filterWidth / 2;
-   int k, l;
-   float sum = 0.0f;
+    int halfFilterSize = filterWidth / 2;
+    float sum = 0.0;
 
-   for (k = -halfFilterSize;k <= halfFilterSize;k++) {
-       for (l = -halfFilterSize; l <= halfFilterSize; l++)
-        {
-            if(filter[(k + halfFilterSize) * filterWidth + l + halfFilterSize] != 0)
-            {
-                if (row + k >= 0 && row + k < imageHeight &&
-                    col + l >= 0 && col + l < imageWidth)
-                {
-                    sum += inputImage[(row + k) * imageWidth + col + l] *
-                            filter[(k + halfFilterSize) * filterWidth +
-                                    l + halfFilterSize];
-                }
-            }
+    int idx = get_global_id(0);
+    int idy = get_global_id(1);
+    
+    int row_begin = idy - halfFilterSize >= 0 ? 0 : halfFilterSize - idy;
+    int row_end = idy + halfFilterSize < imageHeight ? filterWidth - 1 : imageHeight - idy;
+    int col_begin = idx - halfFilterSize >= 0 ? 0 : halfFilterSize - idx;
+    int col_end = idx + halfFilterSize < imageWidth ? filterWidth - 1 : imageWidth - idx;
+
+    for (int i = row_begin; i <= row_end; i++) {
+        int row = idy - halfFilterSize + i;
+        int col = idx - halfFilterSize;
+        for (int j = col_begin; j <= col_end; j++) {
+            sum += inputImage[row * imageWidth + col + j] * filter[i * filterWidth + j];
         }
-   }
-   
-   outputImage[row * imageWidth + col] = sum;
+    }
+    
+    outputImage[idy * imageWidth + idx] = sum;
 }
